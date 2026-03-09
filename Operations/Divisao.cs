ced586084d24936a35c17008faf7e5345ac9b774
Gustavo Pasquini <99883235+GustavoPasquiniLucas@users.noreply.github.com>
2026-03-09 13:56:35 -0300

    Add files via upload

---
+++ Operations/Divisao.cs
@@ -0,0 +1,12 @@
+﻿public class Divisao : IOperation
+{
+    public float DoOperation(float a, float b)
+    {
+        return a / b;
+    }
+
+    public string ShowResult(float a, float b, float result)
+    {
+        return $"{a} / {b} = {result}";
+    }
+}
